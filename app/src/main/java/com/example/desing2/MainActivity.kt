9eb130aa37da80ae2e60067ccb782d5fa19de1da
Lenochka Pusichka <darkselena6969@yandex.ru>
2021-11-08 23:21:48 +0300

    Commit two

--- app/src/main/java/com/example/desing2/MainActivity.kt
+++ app/src/main/java/com/example/desing2/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.desing2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
+private const val TAG = "MainActivity"
+private const val MY_FILTER_TAG = "ufilter"
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var topAppBar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity)
+
+
+
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.favorite -> {
+                    // Handle favorite icon press
+                    true
+                }
+                R.id.search -> {
+                    // Handle search icon press
+                    true
+                }
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    true
+                }
+                else -> false
+            }
+        }
+        Log.d(TAG,"onCreate")
+
     }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
+
+
+    }
+
+
+
+
+
 }
+
+
+
